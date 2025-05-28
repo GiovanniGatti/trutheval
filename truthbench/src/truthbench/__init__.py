4a0059e0efd510c74829238c887574d4fb734b0d
Giovanni Gatti Pinheiro <giovanni.gatti.pinheiro@gmail.com>
2025-05-28 13:03:13 +0200

    adding customization docs

--- truthbench/src/truthbench/__init__.py
+++ truthbench/src/truthbench/__init__.py
@@ -1,3 +1,4 @@
 from .truth_pipeline import truth_pipeline
+from .pipeline import Pipeline, Step, Reader, LLM
 
-__all__ = ["truth_pipeline"]
+__all__ = ["truth_pipeline", "Pipeline", "Step", "Reader", "LLM"]
